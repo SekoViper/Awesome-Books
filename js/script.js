0e967583e4d2186c4766c413c4a74506347b9e5f
SekoVper <damehlivingstone1@gmail.com>
2022-10-24 12:57:01 +0100

    Add comments to js file

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
+// check if the localStorage has a item
 let collections = JSON.parse(localStorage.getItem('books-collection')) || [];
 
+// variables
 const bookContainer = document.getElementById('book-detail');
 const form = document.querySelector('form');
 
@@ -17,16 +19,20 @@ function createCollection(book) {
   bookContainer.insertAdjacentHTML('beforeend', content);
 }
 
+// add event listener to button
 function removeBook(id) {
   const btn = document.getElementById(`${id}`);
 
+  // onclick remove item from localStorage and collections
   btn.addEventListener('click', (event) => {
     event.target.parentElement.remove();
     collections = collections.filter((book) => book.id !== id);
+    // add filtered collections to localStorage
     localStorage.setItem('books-collection', JSON.stringify(collections));
   });
 }
 
+// when page loads items when pages loads and add event listener to each button item
 collections.forEach((book) => {
   createCollection(book);
   removeBook(book.id);
@@ -39,12 +45,16 @@ form.addEventListener('submit', (event) => {
   const book = {
     name: form['book-title'].value,
     author: form['book-author'].value,
+    // getting the ID value
     id: collections.length === 0 ? 1 : parseInt(collections.at(-1).id, 10) + 1,
   };
 
+  // add book to the end of the collections
   collections.push(book);
   localStorage.setItem('books-collection', JSON.stringify(collections));
 
+  // function call to create an item
   createCollection(book);
+  // function call to remove item
   removeBook(book.id);
 });

05784eaf0e5f7675c18b46206da1803911e3875c
Kweeka <mtalbi29@yahoo.com>
2022-10-24 12:11:55 +0300

    Fix errors when removing book cards

--- js/script.js
+++ js/script.js
@@ -2,43 +2,49 @@ let collections = JSON.parse(localStorage.getItem('books-collection')) || [];
 
 const bookContainer = document.getElementById('book-detail');
 const form = document.querySelector('form');
-const removeBtn = bookContainer.querySelector('button')
 
 // create collection as page load
-function createCollection (book, index){
-    const content = `
-      <div>
-        <p>${book.name}</p>
-        <p>${book.author}</p>
-        <button id="${index}">Remove</button>
-        <hr>
-      </div>
-    `
-    // append to the DOM
-    bookContainer.insertAdjacentHTML('beforeend', content);
+function createCollection(book) {
+  const content = `
+    <div>
+      <p>${book.name}</p>
+      <p>${book.author}</p>
+      <button id="${book.id}">Remove</button>
+      <hr>
+    </div>
+  `;
+  // append to the DOM
+  bookContainer.insertAdjacentHTML('beforeend', content);
 }
 
-collections.forEach((book, index) => {
-  createCollection(book, index);
-  removeBook(index);
+function removeBook(id) {
+  const btn = document.getElementById(`${id}`);
+
+  btn.addEventListener('click', (event) => {
+    event.target.parentElement.remove();
+    collections = collections.filter((book) => book.id !== id);
+    localStorage.setItem('books-collection', JSON.stringify(collections));
+  });
+}
+
+collections.forEach((book) => {
+  createCollection(book);
+  removeBook(book.id);
 });
 
 form.addEventListener('submit', (event) => {
   // prevent default behavior of form
   event.preventDefault();
-  let book = { name: form["book-title"].value, author: form["book-author"].value };
+
+  const book = {
+    name: form['book-title'].value,
+    author: form['book-author'].value,
+    id: collections.length === 0 ? 1 : parseInt(collections.at(-1).id, 10) + 1,
+  };
+
   collections.push(book);
   localStorage.setItem('books-collection', JSON.stringify(collections));
-  createCollection(book, collections.length+1);
-  removeBook(collections.length+1);
-});
 
-function removeBook (index){
-  const btn = document.getElementById(`${index}`);
-  btn.addEventListener('click', (event) => {
-    event.target.parentElement.remove();
-    collections = collections.filter(bookId => bookId !== index);
-    console.log(collections);
-    localStorage.setItem('books-collection', JSON.stringify(collections));
-  })
-}
+  createCollection(book);
+  removeBook(book.id);
+});
